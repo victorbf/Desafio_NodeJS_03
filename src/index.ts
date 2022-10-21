141a3bc7a07a1c878287f21fb700904011ae54aa
Victor Beirão Fernandes <victorbeiraof@gmail.com>
2022-10-21 16:02:24 -0300

    feat: final

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import readline from "node:readline";
 
 dotenv.config();
 
@@ -33,11 +34,63 @@ app.use(express.json());
  * Server Activation
  */
 
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+ interface Student {
+  name: string;
+  grade: number;
+}
 
+app.listen(PORT, async () => {
+  console.log(`Listening on port ${PORT}`);
 
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
+  // CÓDIGO PARA ATENDER OS REQUERIMENTOS
+  // R01, R02, R03, R04, R05
+
+	const reader = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
 	
+	// R01
+	const question = (question: string): Promise<string> => {
+		return new Promise(resolve => {
+		 reader.question(question, resolve);
+		});
+	}
+
+	// R03
+  const totalStudents = +await question("Quantos alunos? ");
+
+  if (totalStudents <= 0) {
+    return console.log("Me desculpe dave eu acredito que não posso fazer isso");
+  }
+
+  // R02
+  const students: Student[] = [];
+
+  for (let index = 0; index < totalStudents; index++) {
+    const studentIndex = index + 1;
+    const name = await question(`Nome do(a) aluno(a) ${studentIndex} `);
+    const grade = +await question(`Nota do(a) ${name} `);
+
+    students.push({
+      name,
+      grade,
+    });
+  }
+
+  // R04
+  const bestStudent = students.reduce<Student>(
+    (currentStudent, student) => {
+      if (student.grade > currentStudent.grade) {
+        return student;
+      }
+
+      return currentStudent;
+    },
+    students[0],
+  );
+
+  console.log(
+    `${bestStudent.name} tem a maior nota: ${bestStudent.grade}`,
+  );
 });
