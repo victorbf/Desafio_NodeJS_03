a253bb65b5d176d01a143e1a005531ab040c9e41
Victor Beirão Fernandes <victorbeiraof@gmail.com>
2022-10-21 17:34:22 -0300

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ dotenv.config();
  */
 
 if (!process.env.PORT) {
-	process.exit(1);
+  process.exit(1);
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
@@ -34,7 +34,7 @@ app.use(express.json());
  * Server Activation
  */
 
- interface Student {
+interface Student {
   name: string;
   grade: number;
 }
@@ -45,20 +45,20 @@ app.listen(PORT, async () => {
   // CÓDIGO PARA ATENDER OS REQUERIMENTOS
   // R01, R02, R03, R04, R05
 
-	const reader = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	
-	// R01
-	const question = (question: string): Promise<string> => {
-		return new Promise(resolve => {
-		 reader.question(question, resolve);
-		});
-	}
+  const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-	// R03
-  const totalStudents = +await question("Quantos alunos? ");
+  // R01
+  const question = (question: string): Promise<string> => {
+    return new Promise((resolve) => {
+      reader.question(question, resolve);
+    });
+  };
+
+  // R03
+  const totalStudents = +(await question("Quantos alunos? "));
 
   if (totalStudents <= 0) {
     return console.log("Me desculpe dave eu acredito que não posso fazer isso");
@@ -70,7 +70,7 @@ app.listen(PORT, async () => {
   for (let index = 0; index < totalStudents; index++) {
     const studentIndex = index + 1;
     const name = await question(`Nome do(a) aluno(a) ${studentIndex} `);
-    const grade = +await question(`Nota do(a) ${name} `);
+    const grade = +(await question(`Nota do(a) ${name} `));
 
     students.push({
       name,
@@ -79,23 +79,18 @@ app.listen(PORT, async () => {
   }
 
   // R04
-	const defaultStudent: Student = {
-		name: '',
-		grade: 0
-	};
+  const defaultStudent: Student = {
+    name: "",
+    grade: 0,
+  };
 
-  const bestStudent = students.reduce<Student>(
-    (currentStudent, student) => {
-      if (student.grade > currentStudent.grade) {
-        return student;
-      }
+  const bestStudent = students.reduce<Student>((currentStudent, student) => {
+    if (student.grade > currentStudent.grade) {
+      return student;
+    }
 
-      return currentStudent;
-    },
-		defaultStudent
-  );
+    return currentStudent;
+  }, defaultStudent);
 
-  console.log(
-    `${bestStudent.name} tem a maior nota: ${bestStudent.grade}`,
-  );
+  console.log(`${bestStudent.name} tem a maior nota: ${bestStudent.grade}`);
 });
